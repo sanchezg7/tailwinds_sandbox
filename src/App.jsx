ec6ea27a7fa395c7cc646ec80fe6616d0bf27776
ayogsynergy <sanchezg.eng@gmail.com>
2023-02-19 20:59:02 -0500

    with child components

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import './App.css'
 function App() {
 
   return (
-      <div className="w-1/2 h-16 bg-red-500">
-          Hi
+      <div className="bg-yellow-500 w-auto">
+          <div className="h-10 w-10 bg-red-500">1</div>
+          <div className="h-10 w-10 bg-blue-500">2</div>
+          <div className="h-10 w-10 bg-green-500">3</div>
       </div>
   )
 }

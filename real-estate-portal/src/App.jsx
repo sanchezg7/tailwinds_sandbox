faabd625f413d951812d5e5e6de72d1bb19fd435
ayogsynergy <sanchezg.eng@gmail.com>
2023-02-25 20:56:47 -0500

    adding effects to right side of nav bar

--- real-estate-portal/src/App.jsx
+++ real-estate-portal/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 // max-w: max width of some column size
 // h-#: some height
 // w-#: some width
+// space-x-1: give space of children in flexbox
 
 
 function App() {
@@ -20,11 +21,11 @@ function App() {
                           <span className="font-semibold text-gray-500 text-lg">Prime Properties</span>
                       </a>
                   </div>
-                  <div>
-                      <a href="">Home</a>
-                      <a href="">Service</a>
-                      <a href="">About</a>
-                      <a href="">Contact</a>
+                  <div className="flex space-x-1">
+                      <a href="" className="py-4 px-2 border-green-500 border-b-4 text-green-500 font-semibold">Home</a>
+                      <a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-200">Service</a>
+                      <a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-200">About</a>
+                      <a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-200">Contact</a>
                   </div>
               </div>
           </div>

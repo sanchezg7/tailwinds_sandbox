ab076db71bd0d05d6fcec1258572bee13a483358
ayogsynergy <sanchezg.eng@gmail.com>
2023-02-26 10:53:55 -0500

    formatting card and price

--- real-estate-portal/src/Cards.jsx
+++ real-estate-portal/src/Cards.jsx
@@ -1,14 +1,17 @@
 const Cards = () => {
     return (
-        <div className="">
-            <div>
-                <img src="src/assets/home1.jpg" alt="" />
-                <div>
-                    <div>Orchid Villa</div>
-                    <p>Located in the suburbs...</p>
+        <div className="grid p-10 sm:grid-cols-1 md:grid-cols-3">
+            <div className="rounded shadow-lg">
+                <img className="w-full" src="src/assets/home1.jpg" alt="" />
+                <div className="px-6 pt-4">
+                    <div className="font-bold text-xl mb-2">Orchid Villa</div>
+                    <p className="text-gray-700">
+                        Located in the suburbs of California, orchid villa offers luxury with a modern touch.
+                        Equipped with green lighting and a rainwater harvesting system, this property is eco-friendly.
+                    </p>
                 </div>
-                <div>
-                    <span>$450,000</span>
+                <div className="px-6 pt-4 pb-2">
+                    <span className="inline-block text-green-300 font-bold text-sm px-3 py-1 mr-2 mb-2">$4,500,000</span>
                 </div>
                 <div>
                     <span>7 Beds</span>

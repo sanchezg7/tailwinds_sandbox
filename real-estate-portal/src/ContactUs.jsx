37b5162cf405caf5ee12083f0559e2ab58efb55c
ayogsynergy <sanchezg.eng@gmail.com>
2023-02-26 23:20:22 -0500

    wrap items if no space

--- real-estate-portal/src/ContactUs.jsx
+++ real-estate-portal/src/ContactUs.jsx
@@ -6,7 +6,7 @@ const ContactUs =  () => {
             </div>
             <div className="bg-green-300 mt-5 flex p-10 justify-center items-center">
                 <form className="w-full max-w-lg">
-                    <div className="flex mx-3 mb-6">
+                    <div className="flex flex-wrap mx-3 mb-6">
                         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                             <label htmlFor=""
                                    className="block uppercase text-xs text-gray-700 font-bold mb-2 tracking-wider">First Name</label>

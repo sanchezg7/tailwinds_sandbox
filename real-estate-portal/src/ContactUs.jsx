617abd00cdb04c102dda414a34bc3a96ce886329
ayogsynergy <sanchezg.eng@gmail.com>
2023-02-26 23:06:35 -0500

    minor adjustment to contactus form

--- real-estate-portal/src/ContactUs.jsx
+++ real-estate-portal/src/ContactUs.jsx
@@ -6,9 +6,16 @@ const ContactUs =  () => {
             </div>
             <div className="bg-green-300 mt-5 flex p-10 justify-center items-center">
                 <form className="w-full max-w-lg">
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label htmlFor="" className="block uppercase text-xs text-gray-700 font-bold mb-2 tracking-wider">First Name</label>
-                        <input type="text" className="block w-full border border-green-500 px-3 py-4 rounded-md text-gray-700" placeholder="Jane"/>
+                    <div className="flex">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label htmlFor="" className="block uppercase text-xs text-gray-700 font-bold mb-2 tracking-wider">First Name</label>
+                            <input type="text" className="block w-full border border-green-500 px-3 py-4 rounded-md text-gray-700 mb-3" placeholder="Jane"/>
+                            <p className="text-sm text-green-500 italic">Please fill out this field.</p>
+                        </div>
+                        <div className="w-full md:w-1/2 px-3 md:mb-0">
+                            <label htmlFor="" className="block uppercase text-xs text-gray-700 font-bold mb-2 tracking-wider">Last Name</label>
+                            <input type="text" className="block w-full border border-green-500 px-3 py-4 rounded-md text-gray-700" placeholder="Doe"/>
+                        </div>
                     </div>
                 </form>
             </div>

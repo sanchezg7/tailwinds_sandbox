f7602943b539c8fec0e881f8c1413a4a44d8f090
ayogsynergy <sanchezg.eng@gmail.com>
2023-02-26 23:39:39 -0500

    footer part 2

--- real-estate-portal/src/Footer.jsx
+++ real-estate-portal/src/Footer.jsx
@@ -12,6 +12,25 @@ const Footer = () => {
                         </span>
                     </a>
                 </div>
+                <div className="grid grid-cols-2 gap-x-3 gap-y-8 text-sm">
+                    <div className="space-y-3">
+                        <h3 className="text-green-500 mb-2 uppercase tracking-wide">Product</h3>
+                        <ul className="space-y-1">
+                            <li><a href="">Features</a></li>
+                            <li><a href="">Integrations</a></li>
+                            <li><a href="">Pricing</a></li>
+                            <li><a href="">FAQs</a></li>
+                        </ul>
+                    </div>
+                    <div className="space-y-3">
+                        <h3 className="text-green-500 mb-2 uppercase tracking-wide">Company</h3>
+                        <ul className="space-y-1">
+                            <li><a href="">Privacy</a></li>
+                            <li><a href="">Terms of Service</a></li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
         </footer>
         

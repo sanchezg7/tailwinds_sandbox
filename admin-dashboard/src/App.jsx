c37e62c5aff68a04324cb81053c10e8be5e703a3
ayogsynergy <sanchezg.eng@gmail.com>
2023-02-28 21:13:30 -0500

    basic nav header

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -16,45 +16,28 @@ import Footer from "./Footer.jsx";
 // w-#: some width
 // space-x-1: give space of children in flexbox
 
+const NavBar = () => {
+  return (
+    <div className="bg-indigo-900 w-64">
+        <div className="bg-indigo-800">
+            <div className="uppercase text-gray-200 p-5 font-bold text-xl">Super Corps</div>
+        </div>
+    </div>
+  );
+};
+
 
 function App() {
 
   return (
-      <>
-      <nav className="bg-white shadow-lg">
-          <div className="max-w-6xl mx-auto px-4">
-              <div className="flex justify-between">
-                  <div className="flex space-x-7">
-                      <div>
-                          <a href="#" className="flex items-center py-4 px-2">
-                              <img className="h-8 w-8" src="src/assets/house.png" alt="" />
-                              <span className="font-semibold text-gray-500 text-lg">Prime Properties</span>
-                          </a>
-                      </div>
-                      <div className="hidden md:flex space-x-1 items-center">
-                          <a href="" className="py-4 px-2 border-green-500 border-b-4 text-green-500 font-semibold">Home</a>
-                          <a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-200">Services</a>
-                          <a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-200">About</a>
-                          <a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-200">Contact Us</a>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </nav>
-      <main>
-        <Banner />
-        <SiteBanner />
-        <Locations />
-        <div className="mt-5 p-10 flex justify-center">
-            <h2 class="text-3xl text-gray-500 mb-2">Recent Properties</h2>
+      <body className="bg-green-200">
+        <div className="flex">
+            <NavBar />
+            <div>
+                Main content area
+            </div>
         </div>
-        <Cards />
-        <CommissionsBanner />
-        <Testimonials />
-        <ContactUs />
-      </main>
-    <Footer />
-    </>
+      </body>
   )
 }
 

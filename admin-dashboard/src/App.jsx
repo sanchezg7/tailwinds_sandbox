d502a255aa9d71a93927fcbe32704587395d314d
ayogsynergy <sanchezg.eng@gmail.com>
2023-02-28 22:07:36 -0500

    banner

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css'
 import SideNavBar from "./NavBar.jsx";
 import TopNavBar from "./TopNavBar.jsx";
+import Banner from "./Banner.jsx";
 
 // flex-1: grow/shrink according to the width
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <SideNavBar />
             <div className="flex-1">
                 <TopNavBar />
+                <Banner />
             </div>
         </div>
       </body>
--- admin-dashboard/src/Banner.jsx
+++ admin-dashboard/src/Banner.jsx
@@ -2,26 +2,9 @@
 //tracking-tight: letter spacing of an element
 const Banner = () => {
   return (
-    <div className="mt-10 px-4 md:mt-16">
-        <div className="sm:text-center lg:text-left">
-            <h1 className="tracking-tight font-extrabold md:text-4xl sm:text-5xl md:text-6xl">
-                <span className="block">Premium Properties</span>
-                <span className="block text-green-500">Non-premium Prices</span>
-            </h1>
-            <p className="mt-3 text-gray-500 md:mt-5 md:text-xl sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto lg:mx-0">
-                At prime properties, we ensure that our clients get the best properties at affordable prices.
-                Luxury is our priority and budget is our constraint.
-                Pick and choose from 1000+ properties across the globe.
-            </p>
-            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center">
-                <div className="md:m-2">
-                    <a href="" className="flex justify-center rounded-md bg-green-500 text-white px-8 py-3">View Properties</a>
-                </div>
-                <div className="md:m-2">
-                    <a href="" className="flex justify-center rounded-md bg-green-100 text-green-700 px-8 py-3">Explore Locations</a>
-                </div>
-            </div>
-        </div>
+    <div className="bg-indigo-200 m-7 p-6">
+        <div className="text-4xl text-gray-800 font-md tracking-tighter">Good morning, Super Inc.!</div>
+        <div className="mt-4">These are the stats for today, have a good day.</div>
     </div>
   );
 };

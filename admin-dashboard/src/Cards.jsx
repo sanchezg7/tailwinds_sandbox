7458cae81de28b3934a239355e02490b4d2dcffc
ayogsynergy <sanchezg.eng@gmail.com>
2023-03-03 20:28:30 -0500

    adding basic cards side by side

--- admin-dashboard/src/Cards.jsx
+++ admin-dashboard/src/Cards.jsx
@@ -2,16 +2,30 @@
 
 const items = [
     {
-        content: "Card 1"
+        title: "super plus",
+        subtitle: "sales",
+        asset: "src/assets/wallet.png",
+        dollarAmt: "$25,647",
+        pctIncrease: "+58%"
     },
     {
-        content: "Card 2"
+        title: "super pro",
+        subtitle: "sales",
+        asset: "src/assets/wallet.png",
+        pctIncrease: "-9%"
     }
 ]
 
-const Card = ({ content }) => {
+const Card = ({ title, subtitle, asset, dollarAmt, pctIncrease }) => {
   return (
-      <div className="card w-1/2 bg-white p-4 m">{content}</div>
+      <div className="card w-1/2 bg-white p-4 m">
+          <div><img className="w-10 h-10" src={asset} alt=""/></div>
+          <div className="font-bold text-xl text-gray-700">{title}</div>
+          <div className="uppercase">{subtitle}</div>
+          <span className="font-bold text-3xl">{dollarAmt}</span>
+          {/*badge component*/}
+          <span className="bg-green-500 rounded-full text-white text-sm font-bold p-1 align-text-top">{pctIncrease}</span>
+      </div>
   );
 };
 
